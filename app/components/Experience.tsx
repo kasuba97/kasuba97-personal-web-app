1428cc5bb6b2d890158463b39259bbcfdc4eab5a
Geoffrey Kasuba Sichali <Kasubasich@gmail.com>
2026-03-21 11:08:50 +0200

    Update Experience.tsx

--- app/components/Experience.tsx
+++ app/components/Experience.tsx
@@ -3,17 +3,23 @@ function Experience() {
     <div className="flex flex-col lg:flex-row space-x-2">
       <p>work</p>
       <div className="flex flex-col lg:flex-row space-x-2">
-        <a
+        <div>
+         <a
           href="https://soundwork.io"
-          target="_blank"
+          target="https://soundwork.io/"
           className="hover:bg-slate-400"
-        >
+         >
           (paused dev) soundwork
         </a>
+<p>web3 sound marketplace</p>
+</div≥
 
-        <a href="/#" className="hover:bg-slate-400">
-          (internship) techsprung
+       <div> <a href="/#" className="hover:bg-slate-400">
+          (completed) techsprung private
         </a>
+<p>business management app</p>
+</div>
+<div><a>(completed) Itrade private</a><p>AI sales representative</p></div>
       </div>
     </div>
   );
