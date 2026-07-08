ca4431a9924b6698ebcef2f57b03e77dc5f80bd9
kasuba97 <kasubasich@gmail.com>
2026-07-08 20:39:27 +0200

    back at it

--- app/components/Experience.tsx
+++ app/components/Experience.tsx
@@ -3,20 +3,29 @@ function Experience() {
     <div className="flex flex-col lg:flex-row space-x-2">
       <p>work</p>
       <div className="flex flex-col lg:flex-row space-x-2">
-        <div><a
-          href="https://soundwork.io"
-          target="https://soundwork.io/"
-          className="hover:bg-slate-400"
-         >(paused dev) soundwork
-        </a>
-<p>web3 sound marketplace</p>
-</div>
+        <div>
+          <a
+            href="https://soundwork.io"
+            target="https://soundwork.io/"
+            className="hover:bg-slate-400"
+          >
+            (paused dev) soundwork
+          </a>
+          <p>web3 sound marketplace</p>
+        </div>
 
-       <div><a href="/#" className="hover:bg-slate-400">(completed) techsprung private
-        </a>
-<p>business management app</p>
-</div>
-<div><ahref="/#" className="hover:bg-slate-400">(completed) Itrade private</a><p>AI sales representative</p></div>
+        <div>
+          <a href="/#" className="hover:bg-slate-400">
+            (completed) techsprung private
+          </a>
+          <p>business management app</p>
+        </div>
+        <div>
+          <a href="/#" className="hover:bg-slate-400">
+            (completed) Itrade private
+          </a>
+          <p>AI sales representative</p>
+        </div>
       </div>
     </div>
   );
